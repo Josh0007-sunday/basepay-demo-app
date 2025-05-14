a796aea7652255b93ab392653a071be6ca5cd859
Joshua sunday <joshuasunday300@gmail.com>
2025-05-14 22:24:43 +0100

    nodemodule

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import inject from '@rollup/plugin-inject'
-import NodeGlobalsPolyfillPlugin from '@esbuild-plugins/node-globals-polyfill'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import  NodeModulesPolyfillPlugin  from '@esbuild-plugins/node-modules-polyfill';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -20,12 +21,14 @@ export default defineConfig({
           process: true,
           buffer: true,
         }) as any,
+        
       ],
     },
   },
   build: {
     rollupOptions: {
       plugins: [
+        NodeModulesPolyfillPlugin(),
         inject({
           Buffer: ['buffer', 'Buffer'],
         }),
